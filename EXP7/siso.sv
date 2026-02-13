fbbefcafe30523840cd2e354816e0b700d34f246
mohakgpec23-cmyk <mohakgp.ec23@bmsce.ac.in>
2026-02-13 20:57:03 +0530

    Update siso.sv

--- EXP7/siso.sv
+++ EXP7/siso.sv
@@ -1,12 +1,4 @@
-//------------------------------------------------------------------------------
-//File       : siso.sv
-//Author     : Krishna Gupta/1BM23EC123
-//Created    : 2026-01-24
-//Module     : siso
-//Project    : SystemVerilog and Verification (23EC6PE2SV),
-//Faculty    : Prof. Ajaykumar Devarapalli
-//Description: Serial In Serial Out Shift Register used for basic functional coverage example.
-//------------------------------------------------------------------------------
+
 
 module siso(
   input clk, rst,
