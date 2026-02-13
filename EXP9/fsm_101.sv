c08724f8f70e9628fa2f6d675c664dea3f000dd4
mohakgpec23-cmyk <mohakgp.ec23@bmsce.ac.in>
2026-02-13 20:58:52 +0530

    Update fsm_101.sv

--- EXP9/fsm_101.sv
+++ EXP9/fsm_101.sv
@@ -1,12 +1,4 @@
-//------------------------------------------------------------------------------
-//File       : fsm_101.sv
-//Author     : Krishna Gupta/1BM23EC123
-//Created    : 2026-01-28
-//Module     : fsm_101
-//Project    : SystemVerilog and Verification (23EC6PE2SV),
-//Faculty    : Prof. Ajaykumar Devarapalli
-//Description: 101 Sequence Detector used for basic functional coverage example.
-//------------------------------------------------------------------------------
+
 
 module fsm_101(
   input logic clk, rst, in,
