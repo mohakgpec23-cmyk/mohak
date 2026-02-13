34d302c66a4730c1c43a1c6a4bd9995e2935f37e
mohakgpec23-cmyk <mohakgp.ec23@bmsce.ac.in>
2026-02-13 20:19:35 +0530

    Update digital_clock (1).sv

--- AAT1/TASK2/digital_clock (1).sv
+++ AAT1/TASK2/digital_clock (1).sv
@@ -1,12 +1,4 @@
-//------------------------------------------------------------------------------
-//File       : digital_clock.sv
-//Author     : Krishna Gupta/1BM23EC123
-//Created    : 2026-01-28
-//Module     : digital_clock
-//Project    : SystemVerilog and Verification (23EC6PE2SV),
-//Faculty    : Prof. Ajaykumar Devarapalli
-//Description: Basic Digital Clock used for basic functional coverage example.
-//------------------------------------------------------------------------------
+
 
 module digital_clock(
   input logic clk,
