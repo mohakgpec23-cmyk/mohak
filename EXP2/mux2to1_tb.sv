4f0fbd734ce6f3c44d111847a33f36d4a6ba16b1
mohakgpec23-cmyk <mohakgp.ec23@bmsce.ac.in>
2026-02-13 20:52:30 +0530

    Update mux2to1_tb.sv

--- EXP2/mux2to1_tb.sv
+++ EXP2/mux2to1_tb.sv
@@ -1,12 +1,3 @@
-//------------------------------------------------------------------------------
-//File       : mux2to1_tb.sv
-//Author     : Krishna Gupta/1BM23EC123
-//Created    : 2026-01-23
-//Module     : tb
-//Project    : SystemVerilog and Verification (23EC6PE2SV),
-//Faculty    : Prof. Ajaykumar Devarapalli
-//Description: 2-input Multiplexer used for basic functional coverage example.
-//------------------------------------------------------------------------------
 
 `timescale 1ns/1ps
 
